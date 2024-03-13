fd008cc0f19bd714b0db10a54c71f73023ba9d09
Tad Scritchfield <tadscritch@gmail.com>
2024-03-12 22:10:40 -0500

    fix styles

--- app/playback/page.tsx
+++ app/playback/page.tsx
@@ -3,8 +3,8 @@ import { ExamplesList } from "@/components/audio/examples"
 export default function IndexPage() {
   return (
     <section className="grid items-center gap-2 px-1 pb-8 pt-2 md:py-1">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <p className="rounded-lg bg-muted p-3 text-sm text-muted-foreground">
+      <div className="flex w-full flex-col items-start gap-2">
+        <p className="w-full rounded-lg bg-muted p-3 text-sm text-muted-foreground">
           ● <strong>mp3</strong> is the <em>overall</em> best choice for audio
           on the web, as it has the best browser compatibility and one of the
           smallest file sizes. However, it will not contain all the audio data
